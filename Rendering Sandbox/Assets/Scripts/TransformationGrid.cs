1c06ddac0025cf755998dc3308b4a717a2e290b6
2x-Helix <82784679+2x-Helix@users.noreply.github.com>
2023-05-28 13:16:36 +1000

    Update to transform matrices
    Updated transforms to use matrix multiplication to apply transforms

--- Rendering Sandbox/Assets/Scripts/TransformationGrid.cs
+++ Rendering Sandbox/Assets/Scripts/TransformationGrid.cs
@@ -8,6 +8,7 @@ public class TransformationGrid : MonoBehaviour
     public int gridResolution = 10;
     Transform[] grid;
     List<Transformation> transformations;
+    Matrix4x4 transformation;
 
     void Awake () {
         grid = new Transform[gridResolution * gridResolution * gridResolution];
@@ -22,19 +23,37 @@ public class TransformationGrid : MonoBehaviour
         transformations = new List<Transformation>();
     }
 
-    void Update() {
-        GetComponents<Transformation>(transformations);
-        for (int i = 0, z = 0; z < gridResolution; z++) {
-            for (int y = 0; y < gridResolution; y++) {
-                for (int x = 0; x < gridResolution; x++, i++) {
+    void Update()
+    {
+        UpdateTransformation();
+        for (int i = 0, z = 0; z < gridResolution; z++)
+        {
+            for (int y = 0; y < gridResolution; y++)
+            {
+                for (int x = 0; x < gridResolution; x++, i++)
+                {
                     grid[i].localPosition = TransformPoint(x, y, z);
                 }
             }
         }
     }
 
+    private void UpdateTransformation()
+    {
+        GetComponents<Transformation>(transformations);
+        if (transformations.Count > 0)
+        {
+            transformation = transformations[0].Matrix;
+            for (int i = 1; i < transformations.Count; i++)
+            {
+                transformation = transformations[i].Matrix * transformation;
+            }
+        }
+    }
+
     Transform CreateGridPoint (int x, int y, int z) {
         Transform point = Instantiate<Transform>(prefab);
+        point.parent = gameObject.GetComponent<Transform>();
         point.localPosition = GetCoordinates(x, y, z);
         point.GetComponent<MeshRenderer>().material.color = new Color (
             (float) x / gridResolution,
@@ -51,14 +70,11 @@ public class TransformationGrid : MonoBehaviour
             y - (gridResolution - 1) * 0.5f, 
             z - (gridResolution - 1) * 0.5f 
         );
-    }
+    }   
 
     /* Apply each transformation to point */
     Vector3 TransformPoint (int x, int y, int z) {
         Vector3 coordinates = GetCoordinates(x, y, z);
-        for (int i = 0; i < transformations.Count; i++) {
-            coordinates = transformations[i].Apply(coordinates);
-        }
-        return coordinates;
+        return transformation.MultiplyPoint(coordinates);
     }
 }
